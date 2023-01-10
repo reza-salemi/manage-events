56363e3b28bee3331f89d6ed2740b7436b17f443
salemi <en.salemi75@gmail.com>
2023-01-10 16:43:59 +0330

    refactor: add some dummy data

--- dummy-data.ts
+++ dummy-data.ts
@@ -1,31 +1,36 @@
+type DateFilter = {
+  year: number;
+  month: number;
+};
+
 const DUMMY_EVENTS = [
   {
     id: "e1",
-    title: "Programming for everyone",
+    title: "Morocco 0 - 0 Croatia",
     description:
-      "Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.",
-    location: "Somestreet 25, 12345 San Somewhereo",
-    date: "2021-05-12",
+      "Morocco and Croatia played out the third goalless draw in 24 hours at the 2022 World Cup as Qatar tournament threw up snoozefest on Wednesday",
+    location: "Qatar",
+    date: "2022-05-12",
     image: "images/morocco.jpg",
     isFeatured: false,
   },
   {
     id: "e2",
-    title: "Networking for introverts",
+    title: "France beat Morocco to reach World Cup Final",
     description:
-      "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
-    location: "New Wall Street 5, 98765 New Work",
-    date: "2021-05-30",
+      "France will Play Argentina in the World Cup final after surviving a breathless semi-final against Morocco",
+    location: "Qatar",
+    date: "2022-05-30",
     image: "images/france.jpg",
     isFeatured: true,
   },
   {
     id: "e3",
-    title: "Networking for extroverts",
+    title: "Emir of Qatar dressed Messi in Black",
     description:
-      "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
-    location: "My Street 12, 10115 Broke City",
-    date: "2022-04-10",
+      "Emir of Qatar dressed Messi in Black before handling over the World Cup",
+    location: "Qatar",
+    date: "2022-12-01",
     image: "images/messi.jpeg",
     isFeatured: true,
   },
@@ -39,7 +44,7 @@ export function getAllEvents() {
   return DUMMY_EVENTS;
 }
 
-export function getFilteredEvents(dateFilter: any) {
+export function getFilteredEvents(dateFilter: DateFilter) {
   const { year, month } = dateFilter;
 
   let filteredEvents = DUMMY_EVENTS.filter((event) => {
@@ -52,6 +57,6 @@ export function getFilteredEvents(dateFilter: any) {
   return filteredEvents;
 }
 
-export function getEventById(id: any) {
+export function getEventById(id: string) {
   return DUMMY_EVENTS.find((event) => event.id === id);
 }
